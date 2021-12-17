7406fb654b7c622a0a249b4cf852b3d28f32217b
9935358984yaar <79708844+9935358984yaar@users.noreply.github.com>
2021-12-17 15:07:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function page_one(){
+    window.location="https://samriddhishah.github.io/Multiple-Obj-Recognition/page_one.html";
+}
+
+function page_two(){
+    window.location="https://samriddhishah.github.io/Multiple-Obj-Recognition/page_two.html";
+}
+
+function page_three(){
+    window.location="https://samriddhishah.github.io/Multiple-Obj-Recognition/page_three.html";
+}
